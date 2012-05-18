5fb4f92bfb167bc2786ba12cec6747594b0b39e3
Daisuke Yamashita <daisuke@cookpad.com>
2012-05-18 16:55:31 +0900

    new rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChankoAbTestingSample::Application.config.secret_token = 'c7272eca25b57db2f738833459cb68fdb60571c0ba317577c0fcbb35bf2512a1bb50cefdba1287bd71c4b582b84a9c4cdad601d8ecdad9a8e8a5b34add4717e8'
